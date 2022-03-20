a85ee44c35e1464826986b634195cc8342442a69
Dhruv <samantdhruv@gmail.com>
2022-03-20 22:46:34 +0530

    refactor : added conditional rendering for sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Footer, Header, LandingContainer, Sidebar, Toast } from "./Components";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import {
   AuthContainer,
   LoginBox,
@@ -10,16 +10,23 @@ import RequireAuth from "./Components/PrivateRoute/RequireAuth";
 import { DashboardPage } from "./Pages/DashboardPage/DashboardPage";
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
       <LandingContainer>
         <Toast />
         <Header />
-        <Sidebar />
+        {location.pathname === "/login" ||
+        location.pathname === "/signup" ? null : (
+          <Sidebar />
+        )}
+
         <Routes>
           <Route element={<RequireAuth />}>
             <Route path="/" element={<DashboardPage />} />
           </Route>
+
           <Route element={<RestrictAuth />}>
             <Route
               path="/login"
--- src/Components/LandingComponent/LandingContainer.jsx
+++ src/Components/LandingComponent/LandingContainer.jsx
@@ -1,5 +1,19 @@
+import { useLocation } from "react-router-dom";
 import "./LandingContainer.css";
 
 export const LandingContainer = ({ children }) => {
-  return <section className={`landing-container`}>{children}</section>;
+  const location = useLocation();
+  console.log(location)
+
+  return (
+    <section
+      className={
+        location.pathname === "/login" || location.pathname === "/signup"
+          ? "landing-container-auth"
+          : "landing-container"
+      }
+    >
+      {children}
+    </section>
+  );
 };
